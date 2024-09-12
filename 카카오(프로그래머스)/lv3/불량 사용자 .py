ab701fb5df695edea98d5377333b72659ca67883
Dan2dani <eldms0406@naver.com>
2024-09-12 20:57:51 +0900

    [add] product를 사용한 예시

--- 카카오(프로그래머스)/lv3/불량 사용자 .py
+++ 카카오(프로그래머스)/lv3/불량 사용자 .py
@@ -33,3 +33,37 @@ def solution(user_id, banned_id):
     dfs(0, visited, banned_user, (), answer)
 
     return len(answer)
+
+
+
+# product를 사용한 예시
+from itertools import product
+
+def solution(user_id, banned_id):
+    answer = set()
+    banned_user = []
+    
+    # 각 banned_id 마다 불량 사용자가 될 수 있는 user_id 담기
+    for b in banned_id:
+        b_l = len(b)
+        b_u = []
+        for u in user_id:
+            flag = False
+            if len(u) == b_l:
+                for bb, uu in zip(b, u):
+                    if bb!=uu and bb!='*':
+                        flag = True
+                        break
+            else:
+                flag = True
+            if flag == False:
+                b_u.append(u)
+        banned_user.append(b_u)
+    
+    pro_banned_user = list(product(*banned_user))
+    
+    for pro in pro_banned_user:
+        if len(pro) == len(set(pro)):
+            answer.add(tuple(sorted(pro)))
+
+    return len(answer)
