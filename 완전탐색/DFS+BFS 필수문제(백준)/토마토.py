3decc7a19e0e1bba7ad39f39399217af64330a44
Dan2dani <eldms0406@naver.com>
2024-09-23 20:38:25 +0900

    [mod] 토마토 - 3차원 배열 사용

--- 완전탐색/DFS+BFS 필수문제(백준)/토마토.py
+++ 완전탐색/DFS+BFS 필수문제(백준)/토마토.py
@@ -2,54 +2,95 @@
 from collections import deque
 from sys import stdin
 
-M, N, H = map(int, stdin.readline().split(' '))
+# 3차원 배열 사용
 
-tomato = []
-
-for _ in range(N*H):
-    tomato.append(list(map(int, stdin.readline().split(' '))))
+M, N, H  = map(int, stdin.readline().split(' '))
+tomato = [[list(map(int, stdin.readline().split(' '))) for _ in range(N)] for _ in range(H)]
 
 q = deque()
 
-for i in range(len(tomato)):
-    for j in range(M):
-        if tomato[i][j] == 1:
-            q.append([i, j, 0])
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if tomato[i][j][k] == 1:
+                q.append([i, j, k, 0])
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 상 하 앞 뒤 좌 우
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
 while q:
-    i, j, cnt = q.popleft()
-    layer = i // N # 현재 층
-    h  = i % N # 층 내의 행
+    i, j, k, cnt = q.popleft()
 
-    for k in range(4):
-        nx = i + dx[k]
-        ny = j + dy[k]
+    for p in range(6):
+        nx = i + dx[p]
+        ny = j + dy[p]
+        nz = k + dz[p]
 
-        if layer *N <= nx < (layer+1) * N and 0<= ny < M:
-            if tomato[nx][ny] == 0:
-                tomato[nx][ny] = 1
-                q.append([nx, ny, cnt+1])
+        if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M:
+            if tomato[nx][ny][nz] == 0:
+                tomato[nx][ny][nz] = 1
+                q.append([nx, ny, nz, cnt + 1])
 
-    if layer > 0:
-        x = N*(layer-1) + h
-        if tomato[x][j] == 0:
-            tomato[x][j] = 1
-            q.append([x, j, cnt+1])
-    
-    if layer < H -1:
-        x = N*(layer+1) + h
-        if tomato[x][j] == 0:
-            tomato[x][j] = 1
-            q.append([x, j, cnt+1])
-
-
-for i in range(len(tomato)):
-    for j in range(M):
-        if tomato[i][j] == 0:
-            print(-1)
-            exit()
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if tomato[i][j][k] == 0:
+                print(-1)
+                exit()
 
 print(cnt)
+
+# 2차원 배열 사용
+# M, N, H = map(int, stdin.readline().split(' '))
+
+# tomato = []
+
+# for _ in range(N*H):
+#     tomato.append(list(map(int, stdin.readline().split(' '))))
+
+# q = deque()
+
+# for i in range(len(tomato)):
+#     for j in range(M):
+#         if tomato[i][j] == 1:
+#             q.append([i, j, 0])
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# while q:
+#     i, j, cnt = q.popleft()
+#     layer = i // N # 현재 층
+#     h  = i % N # 층 내의 행
+
+#     for k in range(4):
+#         nx = i + dx[k]
+#         ny = j + dy[k]
+
+#         if layer *N <= nx < (layer+1) * N and 0<= ny < M:
+#             if tomato[nx][ny] == 0:
+#                 tomato[nx][ny] = 1
+#                 q.append([nx, ny, cnt+1])
+
+#     if layer > 0:
+#         x = N*(layer-1) + h
+#         if tomato[x][j] == 0:
+#             tomato[x][j] = 1
+#             q.append([x, j, cnt+1])
+    
+#     if layer < H -1:
+#         x = N*(layer+1) + h
+#         if tomato[x][j] == 0:
+#             tomato[x][j] = 1
+#             q.append([x, j, cnt+1])
+
+
+# for i in range(len(tomato)):
+#     for j in range(M):
+#         if tomato[i][j] == 0:
+#             print(-1)
+#             exit()
+
+# print(cnt)
